6c7c76853efabfe054577fb4b3bc3d595bf2ff98
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-10-19 10:12:43 +0100

    bugs and formatting

--- src/components/rows/Rows.js
+++ src/components/rows/Rows.js
@@ -1,28 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import axios from "../../requests/axios"
+import React, { useState, useEffect } from "react";
+import axios from "../../requests/axios";
 
 function Rows({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
 
-    const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-    useEffect(() => { 
-        async function fetchData() {
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results)
-            return request;
-        }
-        fetchData();
-    }, [fetchUrl])
-
-    console.log(movies);
-    return (
-        <div>
-            <h3>{title}</h3>
-
-
-
-        </div>
-    )
+  console.log(movies);
+  return (
+    <div>
+      <h3>{title}</h3>
+    </div>
+  );
 }
 
 export default Rows;
--- src/screen/Screen.js
+++ src/screen/Screen.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from "react";
 import Row from "../components/rows/Rows";
 import requests from "../requests/requests";
 
 function Screen() {
-    return (
-        <div className="App">
-            <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
-            <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
-        </div>
-    )
+  return (
+    <div className="App">
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+    </div>
+  );
 }
 
 export default Screen;
