92ec081e9cefdbf2d32b22d1f792b32330ebe12e
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-10-19 05:40:13 +0100

    eslint and rows

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Row from "./components/Rows";
 
 function App() {
   return (
---
+++ src/components/Rows.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Rows({title}) {
+    return (
+        <div>
+            <h3>{title}</h3>
+        </div>
+    )
+}
+
+export default Rows;
